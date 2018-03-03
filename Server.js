b041b7a681627b57d1f33ec99c8f60e80cfec251
Damien Duhamel <d.damien@live.fr>
2018-03-03 20:15:05 +0100

    Changer port

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 http.createServer(function(req, res) {
   const parsedUrl = url.parse(req.url);
